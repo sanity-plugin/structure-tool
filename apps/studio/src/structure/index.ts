b570d4fabc6838368f9d6b7357adc07926d6dbb8
Nisharg Shah <nishargshah3101@gmail.com>
2026-05-16 04:41:54 +0530

    chore: fix studio title with workspace

--- apps/studio/src/structure/index.ts
+++ apps/studio/src/structure/index.ts
@@ -1,9 +1,10 @@
 import { structureToolPlugin } from 'sanity-plugin-structure-tool';
 
 import { userRoles, workspaceTypes } from '@/constants/common';
+import workspaces from '@/workspace/workspaces';
 
 export const { structure, templates, defineListItems } = structureToolPlugin({
-  title: 'Sanity Structure Tool Title',
+  title: ({ workspace }) => workspaces.find((item) => item.name === workspace)?.title ?? '',
   roles: Object.values(userRoles),
   defaultRoles: [userRoles.ADMINISTRATOR],
   workspaces: Object.values(workspaceTypes),
