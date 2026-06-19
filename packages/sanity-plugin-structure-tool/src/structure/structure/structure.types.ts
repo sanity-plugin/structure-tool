e8bd29a01d6eba7c3bf82b0dc9ae80a36e98db7d
Nisharg Shah <nishargshah3101@gmail.com>
2026-06-19 23:37:16 +0530

    fix: type issues

--- packages/sanity-plugin-structure-tool/src/structure/structure/structure.types.ts
+++ packages/sanity-plugin-structure-tool/src/structure/structure/structure.types.ts
@@ -4,12 +4,12 @@ import type {
   StructureToolWorkspaceParams,
 } from '@/structure/structureToolPlugin/structureToolPlugin.types';
 import type { StructureToolParams } from '@/structure/types/common.types';
+import type { ListItem } from '@/structure/types/listItem.types';
 import type { ListItemWithWorkspacesAndRoles } from '@/structure/types/listItemCore.types';
-import type { ListItem } from '@/types';
 import type { SimpleMerge } from '@/types/lib.types';
 
 interface StructureListItems<T extends StructureToolParams> {
-  listItems: SimpleMerge<[ListItem<T>, ListItemWithWorkspacesAndRoles<T>]>;
+  listItems: SimpleMerge<[ListItem<T>, ListItemWithWorkspacesAndRoles<T>]>[];
 }
 
 export type StructureListItemsParams<T extends StructureToolParams> = SimpleMerge<
