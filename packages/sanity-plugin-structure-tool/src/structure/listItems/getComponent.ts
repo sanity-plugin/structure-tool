44832acbc1013490e7f0c65129b1e5801ae84d0b
Nisharg Shah <nishargshah3101@gmail.com>
2026-06-22 02:05:05 +0530

    feat: getComponent completed

---
+++ packages/sanity-plugin-structure-tool/src/structure/listItems/getComponent.ts
@@ -0,0 +1,38 @@
+import { generateId } from '@/helpers/generateId';
+import { getComputedListItems } from '@/helpers/getComputedListItems';
+
+import type { ListItemKey } from '@/structure/listItems/listItems.types';
+
+export const getComponent: ListItemKey = (params) => {
+  const { listItemsParams, mappingParams } = params;
+  const { S, context } = listItemsParams;
+  const { listItem } = mappingParams;
+  const { icon, component, ...restListItem } = listItem;
+
+  const {
+    title = '',
+    componentOptions,
+    menuItemGroups = [],
+    menuItems,
+  } = getComputedListItems({ listItem: restListItem, context });
+
+  const { id } = generateId(title, params);
+
+  return S.listItem()
+    .title(title)
+    .id(id)
+    .icon(icon)
+    .showIcon(icon !== false)
+    .child(() => {
+      let schemaBuilder = S.component(component)
+        .id(id)
+        .options({ ...componentOptions })
+        .menuItemGroups(menuItemGroups);
+
+      if (menuItems) {
+        schemaBuilder = schemaBuilder.menuItems(menuItems);
+      }
+
+      return schemaBuilder;
+    });
+};
